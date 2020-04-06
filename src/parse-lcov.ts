1e129846a0a324ea399d55b85bc85a3ee4e35735
Markis Taylor <m@rkis.cc>
2020-04-06 11:46:08 -0400

    Format with prettier

--- src/parse-lcov.ts
+++ src/parse-lcov.ts
@@ -1,66 +1,76 @@
-import { exists, readFile } from 'fs';
-import { Coverage, CoverageCollection } from './coverage-info';
+import { exists, readFile } from "fs";
+import { Coverage, CoverageCollection } from "./coverage-info";
 
 function walkFile(str: string): Promise<CoverageCollection> {
   return new Promise((resolve, reject) => {
-    let data: CoverageCollection = []
+    let data: CoverageCollection = [];
     let item: Coverage | undefined;
-    const lines = ['end_of_record'].concat(str.split('\n'));
+    const lines = ["end_of_record"].concat(str.split("\n"));
 
     for (let line of lines) {
       line = line.trim();
-      const allparts = line.split(':');
+      const allparts = line.split(":");
       const metrics = allparts.shift();
-      const args = allparts.join(':');
-      const parts = [allparts.shift(), allparts.join(':')];
+      const args = allparts.join(":");
+      const parts = [allparts.shift(), allparts.join(":")];
 
       if (item) {
         switch (metrics && metrics.toUpperCase()) {
-          case 'TN': {
+          case "TN": {
             item.title = args.trim();
             break;
-          } case 'SF': {
+          }
+          case "SF": {
             item.file = args.trim();
             break;
-          } case 'FNF': {
+          }
+          case "FNF": {
             item.functions.found = Number(args.trim());
             break;
-          } case 'FNH': {
+          }
+          case "FNH": {
             item.functions.hit = Number(args.trim());
             break;
-          } case 'LF': {
+          }
+          case "LF": {
             item.lines.found = Number(args.trim());
             break;
-          } case 'LH': {
+          }
+          case "LH": {
             item.lines.hit = Number(args.trim());
             break;
-          } case 'DA': {
+          }
+          case "DA": {
             if (item) {
-              const details = args.split(',');
+              const details = args.split(",");
               item.lines.details.push({
                 line: Number(details[0]),
                 hit: Number(details[1])
-              })
+              });
             }
             break;
-          } case 'BRF': {
+          }
+          case "BRF": {
             item.branches.found = Number(parts[1]);
             break;
-          } case 'BRH': {
+          }
+          case "BRH": {
             item.branches.hit = Number(parts[1]);
             break;
-          } case 'FN': {
+          }
+          case "FN": {
             if (item) {
-              const fn = args.split(',');
+              const fn = args.split(",");
               item.functions.details.push({
                 name: fn[1],
                 line: Number(fn[0])
               });
             }
             break;
-          } case 'FNDA': {
+          }
+          case "FNDA": {
             if (item) {
-              const fn = args.split(',');
+              const fn = args.split(",");
               item.functions.details.some((i, k) => {
                 if (item && item.functions.details) {
                   if (i.name === fn[1] && i.hit === undefined) {
@@ -72,14 +82,15 @@ function walkFile(str: string): Promise<CoverageCollection> {
               });
             }
             break;
-          } case 'BRDA': {
+          }
+          case "BRDA": {
             if (item) {
-              const fn = args.split(',');
+              const fn = args.split(",");
               item.branches.details.push({
                 line: Number(fn[0]),
                 block: Number(fn[1]),
                 branch: Number(fn[2]),
-                hit: ((fn[3] === '-') ? 0 : Number(fn[3]))
+                hit: fn[3] === "-" ? 0 : Number(fn[3])
               });
             }
             break;
@@ -87,11 +98,11 @@ function walkFile(str: string): Promise<CoverageCollection> {
         }
       }
 
-      if (line.indexOf('end_of_record') > -1) {
+      if (line.indexOf("end_of_record") > -1) {
         item && data.push(item);
         item = {
-          file: '',
-          title: '',
+          file: "",
+          title: "",
           lines: {
             found: 0,
             hit: 0,
@@ -114,19 +125,23 @@ function walkFile(str: string): Promise<CoverageCollection> {
     if (data.length) {
       resolve(data);
     } else {
-      reject('Failed to parse string');
+      reject("Failed to parse string");
     }
   });
-};
+}
 
 export function parse(file: string): Promise<CoverageCollection> {
   return new Promise((resolve, reject) => {
     exists(file, exists => {
-      !exists ?
-        walkFile(file).then(resolve).catch(reject) :
-        readFile(file, 'utf8', (_, str) => {
-          walkFile(str).then(resolve).catch(reject);
-        });
+      !exists
+        ? walkFile(file)
+            .then(resolve)
+            .catch(reject)
+        : readFile(file, "utf8", (_, str) => {
+            walkFile(str)
+              .then(resolve)
+              .catch(reject);
+          });
     });
   });
-};
+}
