b274c26d4ea7b98605a695789fae5cc2c7cbdf72
markis <m@rkis.cc>
2021-12-24 11:43:32 -0500

    Add show/hide coverage commands (#73)
    This introduces commands to the extension. The first two commands will "hide" and "show" the diagnostics. It hides/shows the diagnostics by clearing them out and then repopulating them.
    
    Fixes #38

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -1,38 +1,66 @@
-import * as assert from 'assert';
-import { join } from 'path';
-import { commands, extensions, languages, StatusBarItem, Uri, window, workspace } from 'vscode';
+import * as assert from "assert";
+import { join } from "path";
+import {
+  commands,
+  extensions,
+  languages,
+  StatusBarItem,
+  Uri,
+  window,
+  workspace,
+  Extension,
+} from "vscode";
 
-suite('code-coverage', () => {
-  const exampleWorkspace = join(__dirname, '../../../', 'example');
+suite("code-coverage", function () {
+  this.timeout(10000);
+  const exampleWorkspace = join(__dirname, "../../../", "example");
   const exampleWorkspaceUri = Uri.file(exampleWorkspace);
-  const exampleIndexFile = join(exampleWorkspace, 'index.ts');
-  const extension = extensions.getExtension("markis.code-coverage");
+  const exampleIndexUri = Uri.file(join(exampleWorkspace, "index.ts"));
+  let extension: Extension<any> | undefined;
+  let onCommand: (cmd: string) => Promise<void> | undefined;
 
   setup(async () => {
     // Open the example workspace and open the example index file
-    await commands.executeCommand('vscode.openFolder', exampleWorkspaceUri);
-    const doc = await workspace.openTextDocument(exampleIndexFile);
+    await commands.executeCommand("vscode.openFolder", exampleWorkspaceUri);
+    const doc = await workspace.openTextDocument(exampleIndexUri);
     await window.showTextDocument(doc);
+    extension = extensions.getExtension("markis.code-coverage");
+    onCommand = extension?.exports.onCommand;
   });
 
   teardown(async () => {
     // All done, close the editor
-    commands.executeCommand('workbench.action.closeActiveEditor');
+    commands.executeCommand("workbench.action.closeActiveEditor");
   });
 
-  test('check diagnostics exist', async () => {
+  test("check diagnostics exist", async () => {
     // Check to see if the diagnostics exist for the example file
     // there should only be one line not covered, and so only one diagnostic should exist
-    const diagnostics = languages.getDiagnostics(Uri.file(exampleIndexFile));
+    const diagnostics = languages.getDiagnostics(exampleIndexUri);
     assert.strictEqual(diagnostics.length, 1);
     assert.strictEqual(diagnostics[0].range.start.line, 5);
   });
 
-  test('check status bar', async () => {
+  test("check status bar", async () => {
     // Check to see if the status bar is updated correctly
     // the example coverage should cover 3 out of 4 lines - "3/4"
     const statusBar: StatusBarItem = extension?.exports.statusBar;
     assert.ok(statusBar.text);
-    assert.ok(statusBar.text.includes('3/4'))
+    assert.ok(statusBar.text.includes("3/4"));
+  });
+
+  test("test commands hide coverage", async () => {
+    // Ensure coverage exists
+    assert.strictEqual(languages.getDiagnostics(exampleIndexUri).length, 1);
+
+    // Hide coverage
+    await onCommand("code-coverage.hide");
+    assert.strictEqual(languages.getDiagnostics(exampleIndexUri).length, 0);
+  });
+
+  test("test commands show coverage", async () => {
+    // Show coverage
+    await onCommand("code-coverage.show");
+    assert.strictEqual(languages.getDiagnostics(exampleIndexUri).length, 1);
   });
 });
