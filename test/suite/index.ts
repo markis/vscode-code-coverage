4417c126fc59b8466c7c2393cfd44e96dde3f38f
Markis Taylor <m@rkis.net>
2023-08-16 18:52:10 -0400

    update the things

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,6 +1,6 @@
-import * as path from "path";
+import { resolve } from "path";
 import Mocha from "mocha";
-import glob from "glob";
+import { glob } from "glob";
 
 export function run(
   testsRoot: string,
@@ -12,13 +12,9 @@ export function run(
     ui: "tdd",
   });
 
-  glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
-    if (err) {
-      return cb(err);
-    }
-
+  glob("**/**.test.js", { cwd: testsRoot }).then(files => {
     // Add files to the test suite
-    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+    files.forEach((f) => mocha.addFile(resolve(testsRoot, f)));
 
     try {
       // Run the mocha test
@@ -28,6 +24,10 @@ export function run(
     } catch (err) {
       console.error(err);
       cb(err);
+    }
+  }).catch(err => {
+    if (err) {
+      return cb(err);
     }
   });
 }
