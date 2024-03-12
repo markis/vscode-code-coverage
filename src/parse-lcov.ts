9658581af3b3c1f3a5840f3946eca9c75bb39623
Markis Taylor <m@rkis.net>
2024-03-11 22:42:48 -0400

    refactor: parse-lcov

--- src/parse-lcov.ts
+++ src/parse-lcov.ts
@@ -1,141 +1,121 @@
 import { existsSync, readFile } from "fs";
 import { Coverage, CoverageCollection } from "./coverage-info";
 
-function walkFile(str: string): Promise<CoverageCollection> {
-  return new Promise((resolve, reject) => {
-    let data: CoverageCollection = [];
-    let item: Coverage | undefined;
-    const lines = ["end_of_record"].concat(str.split("\n"));
+type MetricsMap = {
+  [key: string]: (item: Coverage, args: string) => void;
+};
 
-    for (let line of lines) {
-      line = line.trim();
-      const allparts = line.split(":");
-      const metrics = allparts.shift();
-      const args = allparts.join(":");
-      const parts = [allparts.shift(), allparts.join(":")];
-
-      if (item) {
-        switch (metrics && metrics.toUpperCase()) {
-          case "TN": {
-            item.title = args.trim();
-            break;
-          }
-          case "SF": {
-            item.file = args.trim();
-            break;
-          }
-          case "FNF": {
-            item.functions.found = Number(args.trim());
-            break;
-          }
-          case "FNH": {
-            item.functions.hit = Number(args.trim());
-            break;
-          }
-          case "LF": {
-            item.lines.found = Number(args.trim());
-            break;
-          }
-          case "LH": {
-            item.lines.hit = Number(args.trim());
-            break;
-          }
-          case "DA": {
-            if (item) {
-              const details = args.split(",");
-              item.lines.details.push({
-                line: Number(details[0]),
-                hit: Number(details[1]),
-              });
-            }
-            break;
-          }
-          case "BRF": {
-            item.branches.found = Number(parts[1]);
-            break;
-          }
-          case "BRH": {
-            item.branches.hit = Number(parts[1]);
-            break;
-          }
-          case "FN": {
-            if (item) {
-              const fn = args.split(",");
-              item.functions.details.push({
-                name: fn[1],
-                line: Number(fn[0]),
-              });
-            }
-            break;
-          }
-          case "FNDA": {
-            if (item) {
-              const fn = args.split(",");
-              item.functions.details.some((i, k) => {
-                if (item && item.functions.details) {
-                  if (i.name === fn[1] && i.hit === undefined) {
-                    item.functions.details[k].hit = Number(fn[0]);
-                    return true;
-                  }
-                }
-                return false;
-              });
-            }
-            break;
-          }
-          case "BRDA": {
-            if (item) {
-              const fn = args.split(",");
-              item.branches.details.push({
-                line: Number(fn[0]),
-                block: Number(fn[1]),
-                branch: Number(fn[2]),
-                hit: fn[3] === "-" ? 0 : Number(fn[3]),
-              });
-            }
-            break;
-          }
-        }
-      }
-
-      if (line.indexOf("end_of_record") > -1) {
-        item && data.push(item);
-        item = {
-          file: "",
-          title: "",
-          lines: {
-            found: 0,
-            hit: 0,
-            details: [],
-          },
-          functions: {
-            hit: 0,
-            found: 0,
-            details: [],
-          },
-          branches: {
-            hit: 0,
-            found: 0,
-            details: [],
-          },
-        };
-      }
-    }
-
-    if (data.length) {
-      resolve(data);
-    } else {
-      reject("Failed to parse string");
-    }
-  });
+/**
+ * @description This method creates a new Coverage object with default values.
+ */
+function createCoverageItem(): Coverage {
+  return {
+    file: "",
+    title: "",
+    lines: {
+      found: 0,
+      hit: 0,
+      details: [],
+    },
+    functions: {
+      hit: 0,
+      found: 0,
+      details: [],
+    },
+    branches: {
+      hit: 0,
+      found: 0,
+      details: [],
+    },
+  };
 }
 
-export function parse(file: string): Promise<CoverageCollection> {
+/**
+ * @description This object maps lcov metrics to a handler function.
+ */
+const metricsMap: MetricsMap = {
+  TN: (item: Coverage, args: string) => (item.title = args.trim()),
+  SF: (item: Coverage, args: string) => (item.file = args.trim()),
+  LF: (item: Coverage, args: string) =>
+    (item.lines.found = Number(args.trim())),
+  LH: (item: Coverage, args: string) => (item.lines.hit = Number(args.trim())),
+  DA: (item: Coverage, args: string) => {
+    const details = args.split(",");
+    item.lines.details.push({
+      line: Number(details[0]),
+      hit: Number(details[1]),
+    });
+  },
+  FNF: (item: Coverage, args: string) =>
+    (item.functions.found = Number(args.trim())),
+  FNH: (item: Coverage, args: string) =>
+    (item.functions.hit = Number(args.trim())),
+  FNDA: (item: Coverage, args: string) => {
+    const details = args.split(",");
+    item.functions.details.push({
+      line: Number(details[0]),
+      name: details[1],
+    });
+  },
+  BRF: (item: Coverage, args: string) =>
+    (item.branches.found = Number(args.trim())),
+  BRH: (item: Coverage, args: string) =>
+    (item.branches.hit = Number(args.trim())),
+  BRDA: (item: Coverage, args: string) => {
+    const details = args.split(",");
+    item.branches.details.push({
+      line: Number(details[0]),
+      block: Number(details[1]),
+      branch: Number(details[2]),
+      hit: details[3] === "-" ? 0 : Number(details[3]),
+    });
+  },
+};
+
+/**
+ * @description Parses a string of lcov data into a CoverageCollection
+ * @param str The string to parse
+ * @returns A CoverageCollection
+ */
+function parseFile(str: string): CoverageCollection {
+  let data: CoverageCollection = [];
+  let item: Coverage = createCoverageItem();
+  const lines = str.split("\n");
+
+  for (let line of lines) {
+    line = line.trim();
+    const allparts = line.split(":");
+    const metrics = allparts.shift();
+    const args = allparts.join(":");
+
+    if (item && metrics) {
+      const handler = metricsMap[metrics.toUpperCase()];
+      if (handler) {
+        handler(item, args);
+      }
+    }
+
+    if (line.indexOf("end_of_record") > -1) {
+      item && data.push(item);
+      item = createCoverageItem();
+    }
+  }
+
+  return data;
+}
+
+/**
+ * @description Parses a file of lcov data into a CoverageCollection
+ * @param file The file to parse
+ * @returns A CoverageCollection
+ * @throws Error if the file does not exist
+ */
+export async function parse(file: string): Promise<CoverageCollection> {
   return new Promise((resolve, reject) => {
     !existsSync(file)
-      ? walkFile(file).then(resolve).catch(reject)
+      ? reject(new Error(`File not found: ${file}`))
       : readFile(file, "utf8", (_, str) => {
-          walkFile(str).then(resolve).catch(reject);
-        });
+        resolve(parseFile(str));
+      });
   });
 }
